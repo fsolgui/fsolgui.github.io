3d8e7c19f0bbc83beaad20c8375563236a92dbb3
fsolgui <ferransolergl@gmail.com>
2021-09-26 11:56:01 +0200

    scale

--- threejs/Pracs/proyectoFinal.js
+++ threejs/Pracs/proyectoFinal.js
@@ -186,7 +186,7 @@ function loadScene(){
     var loader = new THREE.GLTFLoader();
     loader.load("../models/laptop_icon/scene.gltf", function(obj){
         obj.scene.rotation.y = -Math.PI / 2;
-        obj.scene.scale.set(0.25,0.25,0.25);
+        obj.scene.scale.set(0.22,0.22,0.25);
         obj.scene.position.set(-4,22,0);
         mesitaCajon.add(obj.scene);});
     scene.add(mesitaCajon);
